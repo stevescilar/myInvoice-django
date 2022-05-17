c8847d50ecbd9a75957807513457118af7ae9bc8
stevescilar <stevemuambi@gmail.com>
2022-05-17 13:18:45 +0300

    updated

--- invoice/models.py
+++ invoice/models.py
@@ -3,12 +3,14 @@ from django.db import models
 # Create your models here.
 class Client(models.Model):
     clientName = models.CharField(null=True,blank=True,max_length=200)
+    phone_number    = models.CharField(max_length=50,unique=True,null=True)
+    email = models.EmailField(max_length=100, unique=True,null=True)
 
     # Utility fields
     uniqueId = models.CharField(null=True, blank=True, max_length=100)
     slug = models.SlugField(max_length=500, unique=True, blank=True, null=True)
-    date_created = models.DateTimeField(blank=True, null=True)
-    last_updated = models.DateTimeField(blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    last_updated = models.DateTimeField(auto_now=True , null=True)
 
 
     def __str__(self):
