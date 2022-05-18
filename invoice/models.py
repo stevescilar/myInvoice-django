9e8fcdf2d546056743950bc68123cfca25ce231c
stevescilar <stevemuambi@gmail.com>
2022-05-18 13:43:50 +0300

    Updated

--- invoice/models.py
+++ invoice/models.py
@@ -1,4 +1,5 @@
 from distutils.command.upload import upload
+from operator import truediv
 from django.db import models
 from django.template.defaultfilters import slugify
 from django.utils import timezone
@@ -35,3 +36,32 @@ class Client(models.Model):
 
         super(Client, self).save(*args, **kwargs)
         
+class Product(models.Model):
+    name = models.CharField(null=True,blank=True, max_length=100)
+    quantity = models.FloatField(null=True,blank=True)
+    description = models.TextField(null=True,blank=True)
+    unit_price = models.FloatField(null=True,blank=True)
+
+    # utility fields
+    uniqueId = models.CharField(null=True, blank=True, max_length=100)
+    slug = models.SlugField(max_length=500, unique=True, blank=True, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    last_updated = models.DateTimeField(auto_now=True , null=True)
+
+    def __str__(self):
+        return '{} {}' . format(self.name, self.uniqueId)
+    
+    def get_absolute_url(self):
+        return reverse ('product-detail', kwargs={'slug':self.slug})
+
+    def save(self,*args,**kwargs):
+        if self.date_created is None:
+            self.date_created = timezone.localtime(timezone.now())
+        if self.uniqueId is None:
+            self.uniqueId = str(uuid4()).split('-')[4]
+            self.slug = slugify('{} {}' . format(self.name, self.uniqueId))
+
+        self.slug = slugify('{} {}' . format(self.name, self.uniqueId))
+        self.last_updated = timezone.localtime(timezone.now())
+
+        super(Product, self).save(*args, **kwargs)
