446012fd5aaaa44a8553698a5aa882cb58ef2458
stevescilar <stevemuambi@gmail.com>
2022-05-18 13:51:56 +0300

    Updated

--- invoice/admin.py
+++ invoice/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from . models import Client
+from . models import Client,Product
 
 
 class ClientAdmin(admin.ModelAdmin):
     list_display = ('clientName','phone_number','email','uniqueId','slug','date_created','last_updated')
-   
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name','quantity','unit_price','description','slug','date_created','last_updated')
 
 admin.site.register(Client,ClientAdmin)
-# Register your models here.
+admin.site.register(Product,ProductAdmin)
