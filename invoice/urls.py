4c0ed20bdfe9c9683d138e4ef5534d672057542d
stevescilar <stevemuambi@gmail.com>
2022-05-22 00:51:24 +0300

    Updated

--- invoice/urls.py
+++ invoice/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('',views.index,name='index'),
     
     path('create_invoice/',views.create_invoice,name='create_invoice'),
-    path('clients',views.clients, name='clients'),
+    path('clients/',views.clients, name='clients'),
     path('create_client/', views.createClient, name='create_client'),
 ]
